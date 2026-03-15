fa6b80b9887c65c56ac47bac8710f2c7c52d315f
Antor Biswas <151869057+antor1010@users.noreply.github.com>
2026-03-16 00:45:13 +0530

    Add files via upload

---
+++ Day_2_variables/sc2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+name="Antor"
+t="Day 2 Bash script"
+u=$(whoami)
+p=$(pwd)
+n=$(hostname -I)
+
+echo $name
+echo $t
+echo "User:",$u
+echo "Path:",$p
+echo "Network:",$n
+
+#Thm room
+
+name="Jammy"
+age=21
+echo "$name is $age years old"
+city="Paris"
+country="France"
+echo $city  			#q2
+echo $country 			#q3
